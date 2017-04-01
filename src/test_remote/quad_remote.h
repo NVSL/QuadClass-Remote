97839abad0ee916a2e54279e4fc84a06a790ecf9
Steven Swanson <swanson@cs.ucsd.edu>
2017-04-01 16:35:55 -0700

    SHow readings on lcd

--- src/test_remote/quad_remote.h
+++ src/test_remote/quad_remote.h
@@ -6,18 +6,17 @@
  * 		Buttons
  * 		Indicator LEDs
  */
-
 #include <Arduino.h>
 
-// Pin definitions for Gimbals (Analog inputs)
-#define PIN_YAW			A0
-#define PIN_THROTTLE 	A1
-#define PIN_ROLL		A2
-#define	PIN_PITCH		A3 
+// Pin definitions for Gimbals (Analog inputs).  It is not clear why the A1, A2,... etc. don't work, but they don't.
+#define PIN_YAW			0 //A0
+#define PIN_THROTTLE 	1// A1 
+#define PIN_ROLL		2// A2 
+#define	PIN_PITCH		3//A3  
 
 // Pin definitions for potentiometers (Analog inputs)
-#define PIN_POT1		A6
-#define PIN_POT2		A7
+#define PIN_POT1		7//A7
+#define PIN_POT2		6//A6
 
 // Pin definitions for digital pins (buttons and indicator LEDs)
 #define PIN_BTN1		16		// PG0 (schematic) G0 (red board)
@@ -25,3 +24,5 @@
 #define PIN_LED_BLUE	22		// PD6 (schematic) D4 (red board)
 #define PIN_LED_GRN		23		// PD5 (schematic) D5 (red board)
 #define PIN_LED_RED		24		// PD4 (schematic) D6 (red board)		
+
+
